bcee65008e525f34b67984fdcadafc4699c55449
Boone B Gorges <boonebgorges@gmail.com>
2023-09-01 10:25:17 -0500

    PHPStan fix.

--- classes/Admin.php
+++ classes/Admin.php
@@ -46,8 +46,7 @@ class Admin {
 	/**
 	 * Displays a welcome panel to introduce users to WordPress.
 	 *
-	 * @since 3.3.0
-	 * @since 5.9.0 Send users to the Site Editor if the active theme is block-based.
+	 * @return void
 	 */
 	public function news_panel() {
 		wp_enqueue_script(
