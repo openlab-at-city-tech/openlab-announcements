9b6936f17b0f5fc4d91bfc96d6460b568ee9c995
Boone B Gorges <boonebgorges@gmail.com>
2023-09-05 11:05:03 -0500

    Asset build.

---
+++ build/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-i18n'), 'version' => 'ef16fdb8e53baf5b78a5');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'openlab-at-city-tech/openlab-announcements',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dfc27325be6f71f93466ee4a60a8443e03643764',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'openlab-at-city-tech/openlab-announcements' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dfc27325be6f71f93466ee4a60a8443e03643764',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
